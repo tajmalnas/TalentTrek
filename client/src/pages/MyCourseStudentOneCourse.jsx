c72dcdeb5f16c3e7c3f2e6a0354655e3082e60fa
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-04-07 06:39:52 +0530

    fix ui

--- client/src/pages/MyCourseStudentOneCourse.jsx
+++ client/src/pages/MyCourseStudentOneCourse.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { useEffect, useState } from "react"
-import { useLocation } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import MCQsTestPage from "./MCQS-test-videocam/MCQ2";
 
 const MyCourseStudentOneCourse = () => {
@@ -33,7 +33,7 @@ const MyCourseStudentOneCourse = () => {
 
   return (
     <div className="text-slate-200 w-full p-5">
-      <div className="text-center text-4xl h-[15vh] w-full flex items-center justify-center bg-[#191b2e] rounded-xl mb-5 font-black tracking-wider">
+      <div className="text-center text-4xl w-full bg-[#191b2e] rounded-xl p-4 font-black tracking-wider">
         {courseData.name}
       </div>
       <div className="flex gap-8 py-4 border-b border-[#2d2f40]">
@@ -65,45 +65,58 @@ const MyCourseStudentOneCourse = () => {
           Final Test
         </button>
       </div>
-      {tab===1 && <div className="flex w-full">
-            {
-                courseData?.videos?.map((video,index) => {
-                    return(
-                        <div key={index} className="w-1/3 p-4">
-                            <div className="bg-gray-800 p-4 rounded-lg">
-                                <video className="w-full h-60">
-                                    <source src={video.videoUrl}
-                                     autoPlay={false}
-                                     type="video/mp4" />
-                                    Your browser does not support the video tag.
-                                </video>
-                                <div className="text-lg font-medium mt-2">{video.videoTitle}</div>
-                            </div>
-                        </div>
-                    )
-                })
-            }
-          </div>
-      }
-      {tab===3 && <MCQsTestPage courseData = {courseData}/>}
-      {tab===2 && <div className="flex w-full">
-            {
-                courseData?.test?.map((question,index) => {
-                    return(
-                        <div key={index} className="w-1/3 p-4">
-                            <div className="bg-gray-800 p-4 rounded-lg">
-                                <div className="text-lg font-medium mt-2">{question.question}</div>
-                                <div className="text-lg font-medium mt-2">{question.options[0]}</div>
-                                <div className="text-lg font-medium mt-2">{question.options[1]}</div>
-                                <div className="text-lg font-medium mt-2">{question.options[2]}</div>
-                                <div className="text-lg font-medium mt-2">{question.options[3]}</div>
-                            </div>
-                        </div>
-                    )
-                })
-            }
-          </div>
-      }
+      {tab === 1 && (
+        <div className="grid grid-cols-3 gap-6 w-full mt-4">
+          {courseData?.videos?.map((video, index) => {
+            return (
+              <div key={index} className="">
+                <div className="bg-gray-800 p-4 rounded-lg">
+                  <video className="w-full h-60 object-cover">
+                    <source
+                      src={video.videoUrl}
+                      autoPlay={false}
+                      type="video/mp4"
+                      className=""
+                    />
+                    Your browser does not support the video tag.
+                  </video>
+                  <div className="text-lg  text-center font-medium mt-4 ">
+                    {video.videoTitle}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      {tab === 3 && <MCQsTestPage courseData={courseData} />}
+      {tab === 2 && (
+        <div className="flex w-full">
+          {courseData?.test?.map((question, index) => {
+            return (
+              <div key={index} className="w-1/3 p-4">
+                <div className="bg-gray-800 p-4 rounded-lg">
+                  <div className="text-lg font-medium mt-2">
+                    {question.question}
+                  </div>
+                  <div className="text-lg font-medium mt-2">
+                    {question.options[0]}
+                  </div>
+                  <div className="text-lg font-medium mt-2">
+                    {question.options[1]}
+                  </div>
+                  <div className="text-lg font-medium mt-2">
+                    {question.options[2]}
+                  </div>
+                  <div className="text-lg font-medium mt-2">
+                    {question.options[3]}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
