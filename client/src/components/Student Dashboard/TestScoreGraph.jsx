ae65a8c8be88595b6e788c91f2bed45b579c1fb3
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-04-07 09:19:28 +0530

    certficate gen

--- client/src/components/Student Dashboard/TestScoreGraph.jsx
+++ client/src/components/Student Dashboard/TestScoreGraph.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import Chart from "chart.js/auto";
+import { Axios } from "axios";
 
 const TestScoreGraph = () => {
   const [testScores, setTestScores] = useState([]);
